5c51fa40675c6687818a5ae48ba0c27651a80928
Fernando Barros <jbarros.1202@gmail.com>
2019-11-08 13:16:51 -0500

    Botones configurados y otras activities creadas

--- app/src/main/java/com/example/amst_l1_barros_espolservicios/MainActivity.java
+++ app/src/main/java/com/example/amst_l1_barros_espolservicios/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.amst_l1_barros_espolservicios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button Sidweb = (Button) findViewById(R.id.BtnSidweb);
+        Sidweb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Sidweb.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        Button Academico = (Button) findViewById(R.id.BtnAcademico);
+        Sidweb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Academico.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        Button Vinculos = (Button) findViewById(R.id.BtnVinculos);
+        Sidweb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Vinculos.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        Button Bienestar = (Button) findViewById(R.id.BtnBienestar);
+        Sidweb.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), Bienestar.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+        }
+
+
+
+
+
 }
